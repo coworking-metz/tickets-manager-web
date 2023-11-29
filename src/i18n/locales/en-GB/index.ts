c5fc8530b73c6246a6b2a14f5dded9ee44d7c4dc
Matthieu Petit <p.matthieu@me.com>
2023-11-29 17:42:24 +0100

    feat(i18n): add missing en-GB translations

--- src/i18n/locales/en-GB/index.ts
+++ src/i18n/locales/en-GB/index.ts
@@ -5,3 +5,7 @@ export { default as navigation } from './navigation.json';
 export { default as action } from './action.json';
 export { default as user } from './user.json';
 export { default as login } from './login.json';
+export { default as validations } from './validations.json';
+export { default as subscriptions } from './subscriptions.json';
+export { default as tickets } from './tickets.json';
+export { default as presences } from './presences.json';
