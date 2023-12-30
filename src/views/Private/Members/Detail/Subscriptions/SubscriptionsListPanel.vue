f7bc33c9290fd0f8dcb777b2a56e7594438fc35b
Matthieu Petit <mtthp@users.noreply.github.com>
2023-12-30 12:37:41 +0100

    fix(router): replace tickets and subscriptions links instead of push (#51)

--- src/views/Private/Members/Detail/Subscriptions/SubscriptionsListPanel.vue
+++ src/views/Private/Members/Detail/Subscriptions/SubscriptionsListPanel.vue
@@ -32,6 +32,7 @@
                 $route.params.subscriptionId === `${subscription._id}` &&
                 $route.name === ROUTE_NAMES.MEMBERS.DETAIL.SUBSCRIPTIONS.DETAIL,
             }"
+            replace
             :to="{
               name: ROUTE_NAMES.MEMBERS.DETAIL.SUBSCRIPTIONS.DETAIL,
               params: { subscriptionId: subscription._id },
@@ -90,6 +91,7 @@
     <div class="mt-auto flex shrink-0 flex-row bg-gray-50 px-4 py-3 sm:px-6">
       <RouterLink
         class="flex flex-row items-center rounded-md border border-gray-300 bg-white px-3 py-2 font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:text-sm"
+        replace
         :to="{ name: ROUTE_NAMES.MEMBERS.DETAIL.SUBSCRIPTIONS.NEW }">
         <SvgIcon aria-hidden="true" class="mr-2 h-5 w-5" :path="mdiPlus" type="mdi" />
         {{ $t('members.detail.orders.subscriptions.add') }}
--- src/views/Private/Members/Detail/Tickets/TicketsListPanel.vue
+++ src/views/Private/Members/Detail/Tickets/TicketsListPanel.vue
@@ -37,6 +37,7 @@
                 $route.params.ticketId === `${ticket._id}` &&
                 $route.name === ROUTE_NAMES.MEMBERS.DETAIL.TICKETS.DETAIL,
             }"
+            replace
             :to="{
               name: ROUTE_NAMES.MEMBERS.DETAIL.TICKETS.DETAIL,
               params: { ticketId: ticket._id },
@@ -84,6 +85,7 @@
     <div class="mt-auto flex flex-row bg-gray-50 px-4 py-3 sm:px-6">
       <RouterLink
         class="flex flex-row items-center rounded-md border border-gray-300 bg-white px-3 py-2 font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:text-sm"
+        replace
         :to="{ name: ROUTE_NAMES.MEMBERS.DETAIL.TICKETS.NEW }">
         <SvgIcon aria-hidden="true" class="mr-2 h-5 w-5" :path="mdiPlus" type="mdi" />
         {{ $t('members.detail.orders.tickets.add') }}
