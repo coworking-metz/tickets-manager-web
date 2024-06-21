b3fbbd826f8b9641653e74d161d5e9b5a043f219
Matthieu Petit <p.matthieu@me.com>
2024-06-21 08:05:58 +0200

    fix(members): add scope to forms

--- src/views/Private/Members/Detail/Subscriptions/SubscriptionsDeleteDialog.vue
+++ src/views/Private/Members/Detail/Subscriptions/SubscriptionsDeleteDialog.vue
@@ -124,7 +124,7 @@ const rules = computed(() => ({
   comment: { required: withAppI18nMessage(required) },
 }));
 
-const vuelidate = useVuelidate(rules, state);
+const vuelidate = useVuelidate(rules, state, { $scope: 'subscriptions-delete' });
 
 const onDelete = async () => {
   const isValid = await vuelidate.value.$validate();
